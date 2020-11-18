009fafa960ba476956d58d7e9ba89a7a53a2cfae
crist <crist@DESKTOP-L210PKJ>
2020-11-18 10:42:12 +0100

    Importacion de javafx.scene con maven conseguida finalmente

--- AppMusic/src/main/java/ProyectoTDS/LogicaNegocio/Reproductor.java
+++ AppMusic/src/main/java/ProyectoTDS/LogicaNegocio/Reproductor.java
@@ -1,9 +1,38 @@
 package ProyectoTDS.LogicaNegocio;
 
 import java.io.File;
-//import javafx.scene.media.Media;
-//import javafx.scene.media.MediaPlayer; 
+import javafx.scene.*;
+
+
 
 public class Reproductor {
+		
+		public Reproductor() {
+			
+		
+		}
+		
+		
+		public void reproducirCancion(Cancion cancion) {
+			/*URL uri = null;
+			try {
+			com.sun.javafx.application.PlatformImpl.startup(() -> {});
+			uri = new URL(url);
+			System.setProperty("java.io.tmpdir", tempPath);
+			Path mp3 = Files.createTempFile("now-playing", ".mp3");
+			System.out.println(mp3.getFileName());
+			try (InputStream stream = uri.openStream()) {
+			Files.copy(stream, mp3, StandardCopyOption.REPLACE_EXISTING);
+			}
+			System.out.println("finished-copy: " + mp3.getFileName());
+			Media media = new Media(mp3.toFile().toURI().toString());
+			mediaPlayer = new MediaPlayer(media);
+			mediaPlayer.play();
+			} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+			} catch (IOException e1) {
+			e1.printStackTrace();
+			}*/
+		}
 
 }
